39d14f41f1fbefdcef433f654383b67e6816e72a
smilingleo <leo.wei.liu@gmail.com>
2015-08-19 11:05:37 +0800

    formatting and comments

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -25,58 +25,64 @@ import play.mvc.Http.RequestBody;
 import play.mvc.Result;
 
 public class Application extends Controller {
-	private static final int AMOUNT = 10;
-	private RequestMeta[] cache = new RequestMeta[AMOUNT];
-	
+    private static final int AMOUNT = 10;
+    private RequestMeta[] cache = new RequestMeta[AMOUNT];
+
     private ReentrantLock lock = new ReentrantLock();
-	
-	public Result index() {
-		return ok("Welcome to Zuora callout demo, view /view page please.");
-	}
-	
-	@BodyParser.Of(BodyParser.TolerantText.class)
-	public Result callout() throws ParserConfigurationException, SAXException, IOException {
+
+    public Result index() {
+        return ok("Welcome to Zuora callout demo, view /view page please.");
+    }
+
+    @BodyParser.Of(BodyParser.TolerantText.class)
+    public Result callout() throws ParserConfigurationException, SAXException, IOException {
         Document doc = null;
         DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
 
         RequestBody body = request().body();
         String xml = body.asText();
-		doc = docBuilder.parse(new InputSource(new ByteArrayInputStream(xml.getBytes("utf-8"))));
-		if (body == null || doc == null)
-			return ok("empty");
-		Map<String, String[]> headers = request().headers();
-		
-		lock.lock();
-		System.arraycopy(cache, 0, cache, 1, AMOUNT - 1); // shift array to right
-		lock.unlock();
-		
-		cache[0] = new RequestMeta();
-		cache[0].setContent(xml);
-		cache[0].setUrlParams(request().queryString());
-		cache[0].setAddress(request().remoteAddress());
-		cache[0].setTimestamp(new Timestamp(System.currentTimeMillis()));
-		if (headers != null)
-			cache[0].setHeaders(headers);
-        
+        doc = docBuilder.parse(new InputSource(new ByteArrayInputStream(xml.getBytes("utf-8"))));
+
+        // you might want to return '40x' instead, but keep in mind Zuora will
+        // retry any failed callout if you enabled callout-retry
+        if (body == null || doc == null)
+            return ok("empty");
+
+        Map<String, String[]> headers = request().headers();
+
+        lock.lock();
+        System.arraycopy(cache, 0, cache, 1, AMOUNT - 1); // shift array to right
+        lock.unlock();
+
+        cache[0] = new RequestMeta();
+        cache[0].setContent(xml);
+        cache[0].setUrlParams(request().queryString());
+        cache[0].setAddress(request().remoteAddress());
+        cache[0].setTimestamp(new Timestamp(System.currentTimeMillis()));
+        if (headers != null)
+            cache[0].setHeaders(headers);
+
         doc.getDocumentElement().normalize();
 
+        // parse the raw xml content
         NodeList paramList = doc.getElementsByTagName("parameter");
-        for (int i=0; i<paramList.getLength(); i++) {
+        for (int i = 0; i < paramList.getLength(); i++) {
             Node node = paramList.item(i);
             if (node.getNodeType() == Node.ELEMENT_NODE) {
-                Element elem = (Element)node;
+                Element elem = (Element) node;
                 String name = elem.getAttribute("name");
                 String value = elem.getTextContent();
                 cache[0].addParam(name, value);
             }
         }
 
+        // only 200 (OK) is considered as a succeeded callout, not '201' or '202'.
         return ok("Got request " + request() + "!");
     }
-	
-	public Result view() {
-		return ok(views.html.index.render(cache));
-	}
+
+    public Result view() {
+        return ok(views.html.index.render(cache));
+    }
 
 }
--- app/models/RequestMeta.java
+++ app/models/RequestMeta.java
@@ -5,62 +5,67 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class RequestMeta implements java.io.Serializable{
+public class RequestMeta implements java.io.Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 2652061019439385069L;
+    private static final long serialVersionUID = 2652061019439385069L;
 
-	private String address;
-	private Timestamp timestamp;
-	private Map<String, String[]> headers = new TreeMap<String, String[]>();
-	private Map<String, String> parsedParams = new HashMap<String, String>();
-	private Map<String, String[]> urlParams = new HashMap<String, String[]>();
-	
-	private String content;
-	
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public Timestamp getTimestamp() {
-		return timestamp;
-	}
-	public void setTimestamp(Timestamp timestamp) {
-		this.timestamp = timestamp;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public Map<String, String[]> getHeaders() {
-		return headers;
-	}
-	
-	public void setHeaders(Map<String, String[]> headers) {
-		this.headers = headers;
-	}
-	public Map<String, String> getParsedParams() {
-		return parsedParams;
-	}
-	
-	public void addHeader(String name, String[] header) {
-		headers.put(name, header);
-	}
-	
-	public void addParam(String name, String value) {
-		parsedParams.put(name, value);
-	}
-	
-	public Map<String, String[]> getUrlParams() {
-		return urlParams;
-	}
-	public void setUrlParams(Map<String, String[]> urlParams) {
-		this.urlParams = urlParams;
-	}
+    private String address;
+    private Timestamp timestamp;
+    private Map<String, String[]> headers = new TreeMap<String, String[]>();
+    private Map<String, String> parsedParams = new HashMap<String, String>();
+    private Map<String, String[]> urlParams = new HashMap<String, String[]>();
+
+    private String content;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Map<String, String[]> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String[]> headers) {
+        this.headers = headers;
+    }
+
+    public Map<String, String> getParsedParams() {
+        return parsedParams;
+    }
+
+    public void addHeader(String name, String[] header) {
+        headers.put(name, header);
+    }
+
+    public void addParam(String name, String value) {
+        parsedParams.put(name, value);
+    }
+
+    public Map<String, String[]> getUrlParams() {
+        return urlParams;
+    }
+
+    public void setUrlParams(Map<String, String[]> urlParams) {
+        this.urlParams = urlParams;
+    }
 }
